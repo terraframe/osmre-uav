255283beaa6794f0eecf80df0eedd903b9616644
Justin Smethie <jsmethie@terraframe.com>
2018-10-30 15:31:12 -0600

    Added a loading bar for communicating with the server

--- uasdm-web/src/main/ng2/src/app/management/management.service.ts
+++ uasdm-web/src/main/ng2/src/app/management/management.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-//import { SiteEntity } from 'angular-tree-component';
+import 'rxjs/add/operator/finally';
+
 import { SiteEntity } from './management';
+import { EventService } from '../event/event.service';
 
 declare var acp: any;
 
 @Injectable()
 export class ManagementService {
 
-    constructor( private http: Http ) { }
+    constructor( private http: Http, private eventService: EventService ) { }
 
     getChildren( id: string ): Promise<SiteEntity[]> {
         let params: URLSearchParams = new URLSearchParams();
@@ -17,7 +19,7 @@ export class ManagementService {
 
 
         return this.http
-            .get( acp + '/project/get-children', {search: params} )
+            .get( acp + '/project/get-children', { search: params } )
             .toPromise()
             .then( response => {
                 return response.json() as SiteEntity[];
@@ -39,9 +41,13 @@ export class ManagementService {
             'Content-Type': 'application/json'
         } );
 
+        this.eventService.start();
 
         return this.http
             .post( acp + '/project/edit', JSON.stringify( { id: id } ), { headers: headers } )
+            .finally(() => {
+                this.eventService.complete();
+            } )            
             .toPromise()
             .then( response => {
                 return response.json() as SiteEntity;
@@ -54,9 +60,13 @@ export class ManagementService {
             'Content-Type': 'application/json'
         } );
 
+        this.eventService.start();
 
         return this.http
             .post( acp + '/project/update', JSON.stringify( { entity: entity } ), { headers: headers } )
+            .finally(() => {
+                this.eventService.complete();
+            } )
             .toPromise()
             .then( response => {
                 return response.json() as SiteEntity;
@@ -69,9 +79,13 @@ export class ManagementService {
             'Content-Type': 'application/json'
         } );
 
+        this.eventService.start();
 
         return this.http
             .post( acp + '/project/new-child', JSON.stringify( { parentId: parentId } ), { headers: headers } )
+            .finally(() => {
+                this.eventService.complete();
+            } )
             .toPromise()
             .then( response => {
                 return response.json() as SiteEntity;
@@ -86,9 +100,13 @@ export class ManagementService {
             'Content-Type': 'application/json'
         } );
 
+        this.eventService.start();
 
         return this.http
             .post( acp + '/project/apply-with-parent', JSON.stringify( { entity: entity, parentId: parentId } ), { headers: headers } )
+            .finally(() => {
+                this.eventService.complete();
+            } )
             .toPromise()
             .then( response => {
                 return response.json() as SiteEntity;
@@ -101,8 +119,13 @@ export class ManagementService {
             'Content-Type': 'application/json'
         } );
 
+        this.eventService.start();
+
         return this.http
             .post( acp + '/project/remove', JSON.stringify( { id: id } ), { headers: headers } )
+            .finally(() => {
+                this.eventService.complete();
+            } )            
             .toPromise()
     }
 }
