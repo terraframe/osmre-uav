b362f594ad605e41ce1ca5e00ea4046b1d039814
nmceachen <nmceachen@terraframe.com>
2018-11-28 12:05:59 -0700

    Implemented first draft of the Workflow data model

---
+++ uasdm-server/src/main/java-gen/stub/gov/geoplatform/uasdm/bus/WorkflowAction.java
@@ -0,0 +1,12 @@
+package gov.geoplatform.uasdm.bus;
+
+public class WorkflowAction extends WorkflowActionBase
+{
+  private static final long serialVersionUID = -893446595;
+  
+  public WorkflowAction()
+  {
+    super();
+  }
+  
+}
