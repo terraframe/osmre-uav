31a8f2280cba597635767c05c7d9cd4aefe36c4e
rrowlands <rrowlands@terraframe.com>
2019-05-03 16:16:18 -0600

    Fixed issues with Zivaro's ODM processing server running out of disk space.

--- src/scripts/install_prod_odm.sh
+++ src/scripts/install_prod_odm.sh
@@ -1,4 +1,4 @@
-# sudo su
+# RUN AS SUDO
 
 # Install Docker (RHEL)
 yum install -y yum-utils device-mapper-persistent-data lvm2
@@ -6,5 +6,14 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install -y --setopt=obsoletes=0 docker-ce-17.03.2.ce-1.el7.centos.x86_64 docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch
 service docker start
 
+# Docker setup
+docker run -v /data/odm:/var/www/datavol -d -p 3000:3000 --restart always --name odm opendronemap/node-opendronemap:0.3.1
 
-docker run -d -p 3000:3000 --restart always --name odm opendronemap/node-opendronemap:0.3.1
+docker exec odm bash -c 'rm -r /var/www/data'
+docker exec odm bash -c 'rm -r /var/www/tmp'
+
+docker exec odm bash -c 'mkdir /var/www/datavol/data'
+docker exec odm bash -c 'mkdir /var/www/datavol/tmp'
+
+docker exec odm bash -c 'ln -s /var/www/datavol/data /var/www/data'
+docker exec odm bash -c 'ln -s /var/www/datavol/tmp /var/www/tmp'
