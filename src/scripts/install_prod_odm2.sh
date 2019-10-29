4c8ab8cda8a41c57068c05e8d9b06a426ccc787f
rrowlands <rrowlands@terraframe.com>
2019-10-28 20:37:10 -0600

    ODM deployment scripts.

--- src/scripts/install_prod_odm2.sh
+++ src/scripts/install_prod_odm2.sh
@@ -14,6 +14,10 @@ service docker start
 # Requires AWS CLI : pip install awscli --upgrade --user
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html
 
+# export AWS_ACCESS_KEY_ID=AKIAIKFVZC4DZ3NIGP4A
+# export AWS_SECRET_ACCESS_KEY=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
+# eval $(aws ecr get-login --no-include-email --region us-west-2)
+
 # Pull the micasense container (our NodeODM might launch it at runtime)
 docker pull 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-micasense:latest
 
@@ -21,9 +25,6 @@ docker pull 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-micasense:latest
 
 ##### These commands are specific to the DEV container!! ####
 
-# Pull & Run the custom UASDM NodeODM container
-# This is the command for the DEV container
-# TODO : The /var/www/data directory needs to be changed (if we want to have separate dev/staging/prod odm containers) but it can't be changed because our micasense ODM plugin has this directory hardcoded into the source. We need to change that source to pull the value from an environment variable and then set it here.
 docker run -d -p 3001:3000 --restart always -v /data/odm/dev/micasense:/opt/micasense -v /data/odm/dev/data:/var/www/datavol -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -e MICASENSE_HOST_BINDING=/data/odm/dev/micasense --name uasdm-nodeodm-dev 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-nodeodm:latest
 
 docker exec uasdm-nodeodm-dev bash -c 'rm -r /var/www/data'
@@ -34,3 +35,18 @@ docker exec uasdm-nodeodm-dev bash -c 'mkdir /var/www/datavol/tmp'
 
 docker exec uasdm-nodeodm-dev bash -c 'ln -s /var/www/datavol/data /var/www/data'
 docker exec uasdm-nodeodm-dev bash -c 'ln -s /var/www/datavol/tmp /var/www/tmp'
+
+
+
+##### These commands are specific to the STAGING container!! ####
+
+docker run -d -p 3002:3000 --restart always -v /data/odm/staging/micasense:/opt/micasense -v /data/odm/staging/data:/var/www/datavol -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -e MICASENSE_HOST_BINDING=/data/odm/staging/micasense --name uasdm-nodeodm-staging 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-nodeodm:latest
+
+docker exec uasdm-nodeodm-staging bash -c 'rm -r /var/www/data'
+docker exec uasdm-nodeodm-staging bash -c 'rm -r /var/www/tmp'
+
+docker exec uasdm-nodeodm-staging bash -c 'mkdir /var/www/datavol/data'
+docker exec uasdm-nodeodm-staging bash -c 'mkdir /var/www/datavol/tmp'
+
+docker exec uasdm-nodeodm-staging bash -c 'ln -s /var/www/datavol/data /var/www/data'
+docker exec uasdm-nodeodm-staging bash -c 'ln -s /var/www/datavol/tmp /var/www/tmp'
