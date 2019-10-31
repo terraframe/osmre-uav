e82f15aaa4efcff7e95848bac6915c15c4008a58
rrowlands <rrowlands@terraframe.com>
2019-10-30 20:20:49 -0600

    Fixed issues with running many multispectral jobs at the same time.

--- uasdm-odm/src/stages/odm_micasense.py
+++ uasdm-odm/src/stages/odm_micasense.py
@@ -20,7 +20,7 @@ class ODMMicasenseStage(types.ODM_Stage):
             tree = outputs['tree']
 
             micasense_host_binding = os.environ['MICASENSE_HOST_BINDING']
-            micasense_working = os.path.abspath('/opt/micasense') # People running this docker image should have bound the micasense_host_binding directory to /opt/micasense.
+            micasense_working = os.path.abspath('/opt/micasense/' + args.name) # People running this docker image should have bound the micasense_host_binding directory to /opt/micasense.
             micain = io.join_paths(micasense_working, 'in')
             micaout = io.join_paths(micasense_working, 'out')
             micathumb = io.join_paths(micaout, 'thumbnails')
@@ -28,37 +28,39 @@ class ODMMicasenseStage(types.ODM_Stage):
             odmImages = tree.input_images # This is the path that ODM expects the input images to exist in (which is our micasense output)
             odmMicasense = io.join_paths(tree.root_path, 'micasense')
 
-            if not io.dir_exists(micain):
-                log.ODM_INFO("Micasense in directory %s doesn't exist. Creating it now. " % micain)
-                system.mkdir_p(micain)
+            shutil.rmtree(micain, ignore_errors=True)
+            shutil.rmtree(micaout, ignore_errors=True)
+            system.mkdir_p(micain)
 
-            log.ODM_INFO("Copying all files in " + odmImages)
-            # Move all images from the odm images directory to our micasense/in directory
-            for filePath in glob.glob(odmImages + '/*.*'):
-                log.ODM_INFO("Moving file " + filePath + " to " + micain + ".")
-                shutil.move(filePath, micain);
+            try:
+                log.ODM_INFO("Copying all files in " + odmImages)
+                # Move all images from the odm images directory to our micasense/in directory
+                for filePath in glob.glob(odmImages + '/*.*'):
+                    log.ODM_INFO("Moving file " + filePath + " to " + micain + ".")
+                    shutil.move(filePath, micain);
 
-            # Wait for the OS to actually move the files...
-            time.sleep(1)
+                # Wait for the OS to actually move the files...
+                time.sleep(1)
 
-            cmd = 'docker run --rm --mount type=bind,src=' + micasense_host_binding + ',dst=/opt/micawork -e MICASENSE_OUT=/opt/micawork/out -e MICASENSE_IN=/opt/micawork/in micasense-docker'
-            log.ODM_INFO("Running command " + cmd)
+                cmd = 'docker run --rm --mount type=bind,src=' + micasense_host_binding + ',dst=/opt/micawork -e MICASENSE_OUT=/opt/micawork/' + args.name + '/out -e MICASENSE_IN=/opt/micawork/' + args.name + '/in uasdm-micasense'
+                log.ODM_INFO("Running command " + cmd)
 
-            #subprocess.check_call(cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.STDOUT)
-            #self.run_command(cmd)
+                #subprocess.check_call(cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.STDOUT)
+                #self.run_command(cmd)
 
-            subprocess.check_call(cmd, shell=True)
+                subprocess.check_call(cmd, shell=True)
 
-            # Move all micasense output images from micasense/out to the odmImages directory
-            for filePath in glob.glob(micathumb + '/*.jpg'):
-                log.ODM_INFO("Copying file " + filePath + " to " + odmImages + ".")
-                shutil.copy(filePath, odmImages);
-                shutil.copy(filePath, odmMicasense);
+                # Move all micasense output images from micasense/out to the odmImages directory
+                for filePath in glob.glob(micathumb + '/*.jpg'):
+                    log.ODM_INFO("Copying file " + filePath + " to " + odmImages + ".")
+                    shutil.copy(filePath, odmImages);
+                    shutil.copy(filePath, odmMicasense);
 
-            log.ODM_INFO("Removing directory " + micain)
-            shutil.rmtree(micain)
-            log.ODM_INFO("Removing directory " + micaout)
-            shutil.rmtree(micaout)
+            finally:
+                log.ODM_INFO("Removing directory " + micain)
+                shutil.rmtree(micain, ignore_errors=True)
+                log.ODM_INFO("Removing directory " + micaout)
+                shutil.rmtree(micaout, ignore_errors=True)
 
     def run_command(self, command):
         log.ODM_INFO("Running command: " + str(command))
