d12a993bd53fd097724ec03aa3d0ba884184d35b
rrowlands <rrowlands@terraframe.com>
2019-10-31 21:54:52 -0600

    CI script

--- ci-build-processing.sh
+++ ci-build-processing.sh
@@ -1,7 +1,7 @@
 
 set -e
 
-sudo rpm -i https://packagecloud.io/github/git-lfs/packages/el/6/git-lfs-2.9.0-1.el6.x86_64.rpm/download
+sudo rpm -i https://packagecloud.io/github/git-lfs/packages/el/6/git-lfs-2.9.0-1.el6.x86_64.rpm/download || true
 
 if [ "$build_micasense" == "true" ]; then
 	cd $WORKSPACE/uasdm/uasdm-micasense
