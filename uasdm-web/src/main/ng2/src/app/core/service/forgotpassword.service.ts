a218ce6400bdcd9c5cda5500c66330f652c8bc3e
Justin Smethie <jsmethie@terraframe.com>
2020-04-21 12:56:26 -0600

    Fixed bug with forgotpassword-complete link not working

--- uasdm-web/src/main/ng2/src/app/core/service/forgotpassword.service.ts
+++ uasdm-web/src/main/ng2/src/app/core/service/forgotpassword.service.ts
@@ -1,27 +1,8 @@
-///
-/// Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
-///
-/// This file is part of Runway SDK(tm).
-///
-/// Runway SDK(tm) is free software: you can redistribute it and/or modify
-/// it under the terms of the GNU Lesser General Public License as
-/// published by the Free Software Foundation, either version 3 of the
-/// License, or (at your option) any later version.
-///
-/// Runway SDK(tm) is distributed in the hope that it will be useful, but
-/// WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/// GNU Lesser General Public License for more details.
-///
-/// You should have received a copy of the GNU Lesser General Public
-/// License along with Runway SDK(tm).  If not, see <ehttp://www.gnu.org/licenses/>.
-///
 
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 import { finalize } from 'rxjs/operators';
-// import 'rxjs/add/operator/toPromise';
 
 import { EventService } from '../../shared/service/event.service'
 
@@ -30,20 +11,35 @@ declare var acp: any;
 @Injectable()
 export class ForgotPasswordService {
 
-    constructor( private http: HttpClient, private eventService: EventService ) { }
+	constructor(private http: HttpClient, private eventService: EventService) { }
 
-    submit( username: string ): Promise<void> {
-        let headers = new HttpHeaders( {
-            'Content-Type': 'application/json'
-        } );
+	submit(username: string): Promise<void> {
+		let headers = new HttpHeaders({
+			'Content-Type': 'application/json'
+		});
 
-        this.eventService.start();
+		this.eventService.start();
 
-        return this.http
-            .post<void>( acp + '/forgotpassword/initiate', JSON.stringify( { username: username } ), { headers: headers } )
+		return this.http
+			.post<void>(acp + '/forgotpassword/initiate', JSON.stringify({ username: username }), { headers: headers })
 			.pipe(finalize(() => {
 				this.eventService.complete();
 			}))
-            .toPromise();
-    }
+			.toPromise();
+	}
+
+	complete(newPassword: string, token: string): Promise<void> {
+		let headers = new HttpHeaders({
+			'Content-Type': 'application/json'
+		});
+
+		this.eventService.start();
+
+		return this.http
+			.post<void>(acp + '/forgotpassword/complete', JSON.stringify({ newPassword: newPassword, token: token }), { headers: headers })
+			.pipe(finalize(() => {
+				this.eventService.complete();
+			}))
+			.toPromise();
+	}
 }
