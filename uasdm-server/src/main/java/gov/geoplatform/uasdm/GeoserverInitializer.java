a8840273318d7b9f2965bc2be02b6621a22a5ed6
Justin Smethie <jsmethie@terraframe.com>
2020-04-24 16:05:56 -0600

    Geoserver initialization change
    Update geoserver to only rebuild the workspace if it doesn't exist.  It
    no longer tears down an existing workspace and rebuilds layers on boot.
    
    Added small UI change to site modal instead of Add a site the text is
    now Add new site
    
    Addresses: https://terraframe.atlassian.net/browse/OU-284
    Addresses: https://terraframe.atlassian.net/browse/OU-292

---
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/GeoserverInitializer.java
@@ -0,0 +1,179 @@
+package gov.geoplatform.uasdm;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.Collection;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.runwaysdk.session.Request;
+
+import it.geosolutions.geoserver.rest.GeoServerRESTReader;
+import net.geoprism.PluginUtil;
+import net.geoprism.context.ServerContextListener;
+import net.geoprism.gis.geoserver.GeoserverFacade;
+import net.geoprism.gis.geoserver.GeoserverInitializerIF;
+import net.geoprism.gis.geoserver.GeoserverProperties;
+
+public class GeoserverInitializer implements UncaughtExceptionHandler, ServerContextListener
+{
+  private static boolean             initialized = false;
+
+  private static final ReentrantLock lock        = new ReentrantLock();
+
+  private static final Logger        initLogger  = LoggerFactory.getLogger(GeoserverInitializer.class);
+
+  private static InitializerThread   initializerThread;
+
+  public static class InitializerThread extends Thread
+  {
+
+    private static final Logger logger = LoggerFactory.getLogger(InitializerThread.class);
+
+    public InitializerThread()
+    {
+      super();
+    }
+
+    @Override
+    public void run()
+    {
+      GeoServerRESTReader reader = GeoserverProperties.getReader();
+
+      while (!Thread.interrupted())
+      {
+        try
+        {
+          lock.lock();
+
+          logger.debug("Attempting to check existence of geoserver");
+
+          if (reader.existGeoserver())
+          {
+            logger.debug("Geoserver available.");
+
+            runInRequest();
+
+            initialized = true;
+            logger.debug("Geoserver initialized.");
+            return; // we are done here
+          }
+          else
+          {
+            try
+            {
+              logger.debug("Waiting for Geoserver to start.");
+              Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+              return;
+            }
+          }
+        }
+        catch (Throwable t)
+        {
+          // we couldn't hit the application correctly, so log the error
+          // and quit the loop to avoid excessive logging
+          logger.error("Unable to start the application.", t);
+          return;
+        }
+        finally
+        {
+          lock.unlock();
+        }
+      }
+    }
+
+    @Request
+    private void runInRequest()
+    {
+      if (!GeoserverFacade.workspaceExists())
+      {
+        logger.info("Geoserver workspace and store not found.  Republishing layers.");
+
+        GeoserverFacade.publishWorkspace();
+        GeoserverFacade.publishStore();
+
+        Collection<GeoserverInitializerIF> initializers = PluginUtil.getGeoserverInitializers();
+
+        for (GeoserverInitializerIF initializer : initializers)
+        {
+          initializer.initialize();
+        }
+      }
+    }
+
+  }
+
+  /**
+   * Log the error.
+   */
+  @Override
+  public void uncaughtException(Thread t, Throwable e)
+  {
+    initLogger.error("Exception occurred in thread [" + t.getName() + "].", e);
+  }
+
+  @Override
+  public void initialize()
+  {
+  }
+
+  @Override
+  public void startup()
+  {
+    initGeoserver();
+  }
+
+  @Override
+  public void shutdown()
+  {
+    cleanup();
+  }
+
+  public static boolean isInitialized()
+  {
+    try
+    {
+      lock.lock();
+
+      return initialized;
+    }
+    finally
+    {
+      lock.unlock();
+    }
+  }
+
+  public static void initGeoserver()
+  {
+    GeoserverInitializer init = new GeoserverInitializer();
+
+    try
+    {
+      initLogger.debug("Attempting to initialize context.");
+
+      // create another thread to avoid blocking the one starting the webapps.
+      initializerThread = new InitializerThread();
+      initializerThread.setUncaughtExceptionHandler(init);
+      initializerThread.setDaemon(true);
+      initializerThread.start();
+
+      initLogger.debug("Context initialized...[" + GeoserverInitializer.class + "] started.");
+    }
+    catch (Throwable t)
+    {
+      initLogger.error("Could not initialize context.", t);
+    }
+  }
+
+  public static void cleanup()
+  {
+    if (initializerThread != null)
+    {
+      initializerThread.interrupt();
+    }
+  }
+}
