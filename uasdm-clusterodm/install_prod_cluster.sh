9c5b1995c3ffbcf9f97fb2849d72442db0407a41
rrowlands <rrowlands@terraframe.com>
2020-05-12 10:18:06 -0600

    Updated prod cluster deployment script.

--- uasdm-clusterodm/install_prod_cluster.sh
+++ uasdm-clusterodm/install_prod_cluster.sh
@@ -22,12 +22,13 @@ export AWS_ACCESS_KEY_ID=AKIAIKFVZC4DZ3NIGP4A
 export AWS_SECRET_ACCESS_KEY=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
 eval $(aws ecr get-login --no-include-email --region us-west-2)
 
+
 ##### These commands are specific to the DEV container!! ####
 
 docker run -d -p 4001:3000 -p 4501:8080 -p 10001:10000 -v /data/odm/config/aws-config-dev.json:/var/www/config-uasdm.json --link uasdm-nodeodm-dev --name uasdm-clusterodm-dev 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-clusterodm:latest --asr /var/www/config-uasdm.json --public-address http://10.120.10.50:4001/
 
 sleep 2;
-{ sleep 1; echo "NODE ADD uasdm-nodeodm 3001"; sleep 1; echo "NODE LOCK 1"; sleep 1; echo "QUIT"; } | telnet localhost 4501
+{ sleep 1; echo "NODE ADD uasdm-nodeodm-dev 3000"; sleep 1; echo "NODE LOCK 1"; sleep 1; echo "QUIT"; } | telnet localhost 4501
 
 
 ##### These commands are specific to the STAGING container!! ####
@@ -35,7 +36,7 @@ sleep 2;
 docker run -d -p 4002:3000 -p 4502:8080 -p 10002:10000 -v /data/odm/config/aws-config-staging.json:/var/www/config-uasdm.json --link uasdm-nodeodm-staging --name uasdm-clusterodm-staging 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-clusterodm:latest --asr /var/www/config-uasdm.json --public-address http://10.120.10.50:4002/
 
 sleep 2;
-{ sleep 1; echo "NODE ADD uasdm-nodeodm 3002"; sleep 1; echo "NODE LOCK 1"; sleep 1; echo "QUIT"; } | telnet localhost 4502
+{ sleep 1; echo "NODE ADD uasdm-nodeodm-staging 3000"; sleep 1; echo "NODE LOCK 1"; sleep 1; echo "QUIT"; } | telnet localhost 4502
 
 
 ##### These commands are specific to the PROD container!! ####
@@ -43,4 +44,4 @@ sleep 2;
 docker run -d -p 4000:3000 -p 4500:8080 -p 10000:10000 -v /data/odm/config/aws-config-prod.json:/var/www/config-uasdm.json --link uasdm-nodeodm-prod --name uasdm-clusterodm-prod 961902606948.dkr.ecr.us-west-2.amazonaws.com/uasdm-clusterodm:latest --asr /var/www/config-uasdm.json --public-address http://10.120.10.50:4000/
 
 sleep 2;
-{ sleep 1; echo "NODE ADD uasdm-nodeodm 3000"; sleep 1; echo "QUIT"; } | telnet localhost 4500
+{ sleep 1; echo "NODE ADD uasdm-nodeodm-prod 3000"; sleep 1; echo "QUIT"; } | telnet localhost 4500
