6cb8adc1cb43eea26cd04fd843088412df7d4e2c
rrowlands <rrowlands@terraframe.com>
2020-10-13 17:10:40 -0600

    Experimental serverless pyvips implementation.

---
+++ uasdm-lambda-thumbnail/python-serverless/README.txt
@@ -0,0 +1,21 @@
+
+# How to Running
+
+serverless deploy
+serverless invoke -f uasdm-lambda-thumbnail-py --log
+
+
+
+# Environment thumbnail3
+conda create -n thumbnail3 -c conda-forge -c bioconda -c defaults python=3.7 pip pyvips 'imagemagick<7.0.9' boto3
+conda activate thumbnail3
+
+
+# After adding a pip dependency:
+pip list --format=freeze > requirements.txt
+
+
+## REFERENCES
+
+# https://www.serverless.com/blog/serverless-python-packaging
+# https://dev.to/aws-heroes/aws-lambda-microservice-workshop-using-s3-libvips-ruby-4o96
---
+++ uasdm-lambda-thumbnail/python-serverless/handler.py
@@ -0,0 +1,111 @@
+
+import boto3
+from io import BytesIO
+import re
+from urllib.parse import unquote_plus
+import pyvips
+
+buffer_size = 10240
+
+def resize_image(bucket, srcKey): 
+	
+	# Calculate our destination
+	dstKey = "";
+	if (srcKey.find("/") != -1):
+		lastIndex = srcKey.rfind("/");
+		lastPeriod = srcKey.rfind(".");
+		dstKey = srcKey[0:lastIndex] + "/thumbnails" + srcKey[lastIndex:lastPeriod] + ".png";
+	else:
+		dstKey = "thumbnails/" + srcKey;
+	
+	# Don't process images inside a 'thumbnails' directory (infinite loop)
+	if (srcKey.find("thumbnails/") != -1):
+		return "Rejecting key [" + srcKey + "] because it is inside the thumbnails directory";
+	
+	# Skip the dem directory
+	if (srcKey.find("dem/") != -1):
+		return "Rejecting key [" + srcKey + "] because it is inside the dem directory";
+
+	# Infer the image type.
+	typeMatch = re.match(".*\.([^.]*)$", srcKey);
+	if (not typeMatch):
+		return "Could not determine the image type on source key: " + srcKey;
+	
+	# Check that the image type is supported
+	imageType = typeMatch.groups()[0].lower();
+	supportedFormats = ['jpeg', 'jpg', 'png', 'gif', 'bmp', 'tif', 'tiff'];
+	try:
+		supportedFormats.index(imageType)
+	except ValueError:
+			return 'Unsupported image type: ' + imageType;
+	
+	print("Will download image from " + srcKey + " and upload to " + dstKey)
+	
+	# Download the image from S3
+	s3 = boto3.resource('s3')
+	response = s3.Object(bucket_name=bucket, key=srcKey).get()
+	streamingDownload = response['Body'] # is of type botocore.response.StreamingBody
+
+	# Define a Vips Reader for the S3 StreamingBody
+	def read_handler(size):
+		return streamingDownload.read(amt=buffer_size)
+	vipsSource = pyvips.SourceCustom()
+	vipsSource.on_read(read_handler)
+	
+	# Define a Vips Writer for the S3 Uploader
+	binary_stream = BytesIO()
+	def write_handler(chunk):
+		return binary_stream.write(chunk)
+	vipsTarget = pyvips.TargetCustom()
+	vipsTarget.on_write(write_handler)
+	
+	# Create a thumbnail
+	image = pyvips.Image.new_from_source(vipsSource, '', access='sequential')
+	#image = pyvips.Image.new_from_file("/home/rich/Documents/scale-test/4.8g.png")
+	image = image.thumbnail_image(300, height=300)
+	image.write_to_target(vipsTarget, imageType)
+	#image.write_to_file("/home/rich/Documents/scale-test/4.8g-thumb.png")
+	
+	# Upload to S3
+	s3.Object(bucket, dstKey).put(Body=binary_stream)
+	
+	return "Success"
+	
+
+# This method is a direct callback from lambda. The goal here is to convert the params
+# From lambda into a generic format which can also be used locally for testing
+def lambda_handler(event, context):
+		msg = "";
+
+		for record in event['Records']:
+		
+				bucket = record['s3']['bucket']['name']
+				srcKey = unquote_plus(record['s3']['object']['key'])
+				
+				msg = msg + resize_image(bucket, srcKey) + "\n";
+		
+		print("Returning with message: " + msg)
+		return {'message': msg};
+
+# This method allows us to run our function locally for testing purposes
+if __name__ == "__main__":
+		class TestContext(object):	# fake object
+				function_name = "++MAIN++"
+
+		test_event = {
+			"Records": [
+				{
+					"s3": {
+						"bucket": {
+							"name": "terraframe-test-bucket"
+						},
+						"object": {
+							"key": "uasdm-lambda-test/230m.png"
+						}
+					}
+				}
+			]
+		}
+		lambda_handler(test_event, TestContext())
+
+				
