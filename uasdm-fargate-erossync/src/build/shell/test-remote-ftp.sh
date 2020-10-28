f64a47e82d72c9d8539598472178fa78d84be638
rrowlands <rrowlands@terraframe.com>
2020-10-28 13:24:29 -0600

    Updated ODM worker to pull images from Zivaro ECR.

--- uasdm-fargate-erossync/src/build/shell/test-remote-ftp.sh
+++ uasdm-fargate-erossync/src/build/shell/test-remote-ftp.sh
@@ -9,7 +9,7 @@ sudo docker rm -f uasdm-fargate-erossync-test-ftp-server || true
 # Boot a test ftp server
 sudo docker run -d --rm --name uasdm-fargate-erossync-test-ftp-server \
             -p 39538:21 \
-            -e FTP_USER=uasdm-dev-test -e FTP_PASSWORD=dy6hZm6V -e HOST=100.24.153.115 \
+            -e FTP_USER=uasdm-dev-test -e FTP_PASSWORD=dy6hZm6V -e HOST=52.0.230.226 \
             -p 65000-65004:65000-65004 \
             -e PASV_MIN_PORT=65000 -e PASV_MAX_PORT=65004 \
             -v /data/ftp:/data \
