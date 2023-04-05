cd76a8beb617f430329bca2217e2fc0e8eb29a78
jsmethie <jsmethie@terraframe.com>
2023-04-05 13:03:05 -0600

    chore: missing file

---
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/processing/PotreeConverterProcessor.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2020 The Department of Interior
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package gov.geoplatform.uasdm.processing;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.runwaysdk.resource.ApplicationFileResource;
+import com.runwaysdk.resource.FileResource;
+
+import gov.geoplatform.uasdm.AppProperties;
+import gov.geoplatform.uasdm.graph.Product;
+import gov.geoplatform.uasdm.model.CollectionIF;
+
+public class PotreeConverterProcessor extends S3FileUpload
+{
+  private Logger logger = LoggerFactory.getLogger(PotreeConverterProcessor.class);
+
+  public PotreeConverterProcessor(String s3Path, Product product, CollectionIF collection, StatusMonitorIF monitor)
+  {
+    super(s3Path, product, collection, monitor);
+  }
+
+  @Override
+  public boolean process(ApplicationFileResource res)
+  {
+    File file = res.getUnderlyingFile();
+
+    final String basename = FilenameUtils.getBaseName(file.getName());
+
+    File outputDirectory = new File(file.getParent(), basename + "-potree");
+    
+    try
+    {
+      // Copy the underlying file to a filename with the proper
+      File input = File.createTempFile("laz", "." + res.getNameExtension());
+
+      // File input = new File(file.getParentFile(), res.getName());
+
+      FileUtils.copyFile(file, input);
+
+      
+      boolean success = new SystemProcessExecutor(this.monitor).execute(new String[] { AppProperties.getPotreeConverterPath(), input.getAbsolutePath(), "-o", outputDirectory.getAbsolutePath() });
+
+      if (success && outputDirectory.exists())
+      {
+        super.process(new FileResource(outputDirectory));
+      }
+      else
+      {
+        logger.info("Problem occurred generating Potree Converter Transform. Potree data directory did not exist at [" + outputDirectory.getAbsolutePath() + "].");
+        monitor.addError("Problem occurred generating gdal transform. Hillshade file did not exist.");
+      }
+
+    }
+    catch (IOException e)
+    {
+      logger.info("Problem occurred generating Potree Converter Transform. Potree data directory did not exist at [" + outputDirectory.getAbsolutePath() + "].");
+      monitor.addError("Problem occurred generating gdal transform. Hillshade file did not exist.");
+    }
+
+    return false;
+  }
+}
