2dbcb9b3dbea73c2c71511532081acbf5f89692e
Richard Rowlands <rrowlands@terraframe.com>
2023-05-15 12:19:18 -0600

    Revert "feat: switch over to ODM multispectral processing"
    This reverts commit 377cb47e2a0b58577c78299d4097dd00ec18ef47.

--- uasdm-server/src/main/java/gov/geoplatform/uasdm/odm/ODMService.java
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/odm/ODMService.java
@@ -198,11 +198,13 @@ public class ODMService implements ODMServiceIF
     dtm.put("value", "true");
     arr.put(dtm);
 
+    // Our custom Micasense multispectral parameter, which only works on our
+    // custom build of ODM.
     if (isMultispectral)
     {
       JSONObject multispectral = new JSONObject();
-      multispectral.put("name", "radiometric-calibration");
-      multispectral.put("value", "camera");
+      multispectral.put("name", "multispectral");
+      multispectral.put("value", String.valueOf(isMultispectral));
       arr.put(multispectral);
     }
 
@@ -214,12 +216,10 @@ public class ODMService implements ODMServiceIF
 
     // Another one of our custom parameters, which only exists on our custom
     // build of NodeODM.
-    /*
     JSONObject joImagesCount = new JSONObject();
     joImagesCount.put("name", "imagesCount");
     joImagesCount.put("value", imagesCount);
     arr.put(joImagesCount);
-    */
 
     if (configuration.getResolution() != null)
     {
