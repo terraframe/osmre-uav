6af34ac86bf7a2886fb1371d2325c14850ca708f
Richard Rowlands <rrowlands@terraframe.com>
2023-12-08 14:24:27 -0700

    chore(keycloak): assign admin role to keycloak admin user

---
+++ uasdm-server/src/main/resources/domain/(0001701883599855)KeycloakAdminUserRole.java
@@ -0,0 +1,18 @@
+<version xsi:noNamespaceSchemaLocation="classpath:com/runwaysdk/resources/xsd/version.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <doIt>
+    <create>
+    	<object key="fc1606c0-d269-3ad0-9130-f76d3100052a" type="net.geoprism.account.ExternalProfile">
+	        <attribute name="firstName" value="admin" />
+	        <attribute name="lastName" value="admin" />
+	        <attribute name="email" value="admin@noreply.com" />
+	        <attribute name="username" value="admin" />
+	        <attribute name="remoteId" value="admin" />
+	        <attribute name="displayName" value="admin" />
+	      </object>
+    </create>
+    <update></update>
+  </doIt>
+  <undoIt>
+    <delete></delete>
+  </undoIt>
+</version>
