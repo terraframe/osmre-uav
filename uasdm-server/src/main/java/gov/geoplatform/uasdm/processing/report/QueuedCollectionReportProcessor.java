4d28e70d1404c5171e0a0212331a7f4c94f97482
jsmethie <jsmethie@terraframe.com>
2024-01-02 15:21:00 -0700

    fix for blocking queue thread preventing shutdown

--- uasdm-server/src/main/java/gov/geoplatform/uasdm/processing/report/QueuedCollectionReportProcessor.java
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/processing/report/QueuedCollectionReportProcessor.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2020 The Department of Interior
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package gov.geoplatform.uasdm.processing.report;
 
@@ -19,23 +19,38 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class QueuedCollectionReportProcessor implements CollectionReportProcessor
 {
-  private static final Logger                 logger    = LoggerFactory.getLogger(QueuedCollectionReportProcessor.class);
+  private static final Logger                 logger = LoggerFactory.getLogger(QueuedCollectionReportProcessor.class);
 
   private ExecutorService                     executor;
 
-  private ExecutorService                     scheduler = Executors.newFixedThreadPool(1);
+  private ExecutorService                     scheduler;
 
   private BlockingQueue<CollectionReportTask> queue;
 
   public QueuedCollectionReportProcessor()
   {
+    this.scheduler = Executors.newFixedThreadPool(1, new ThreadFactory()
+    {
+      @Override
+      public Thread newThread(Runnable r)
+      {
+        Thread thread = Executors.defaultThreadFactory().newThread(r);
+        thread.setName("report-queue");
+        thread.setDaemon(true);
+
+        return thread;
+      }
+    });
+
     this.executor = Executors.newFixedThreadPool(1);
+
     this.queue = new ArrayBlockingQueue<CollectionReportTask>(1000);
 
     this.scheduler.execute(() -> {
