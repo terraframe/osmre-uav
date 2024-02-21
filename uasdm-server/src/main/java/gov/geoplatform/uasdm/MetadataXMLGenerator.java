95b75de5856efc4e539dcf29896f636371792de5
jsmethie <jsmethie@terraframe.com>
2024-02-21 11:26:24 -0700

    chore: Fixed issue where concurrent users writing metadata at the same time were writing/deleting the same temp file.

--- uasdm-server/src/main/java/gov/geoplatform/uasdm/MetadataXMLGenerator.java
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/MetadataXMLGenerator.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2020 The Department of Interior
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package gov.geoplatform.uasdm;
 
@@ -185,7 +185,7 @@ public class MetadataXMLGenerator
       {
         metadata.getSensor().setFocalLength(sensor.getRealFocalLength().toString());
       }
-      
+
     }
 
     return metadata;
@@ -629,34 +629,37 @@ public class MetadataXMLGenerator
 
   private File createTempFile(Document document) throws TransformerFactoryConfigurationError
   {
-    File temp = new File(AppProperties.getTempDirectory(), "metadata.xml");
-
-    try (FileOutputStream fos = new FileOutputStream(temp))
+    try
     {
-      try
-      {
-        Transformer tr = TransformerFactory.newInstance().newTransformer();
-        tr.setOutputProperty(OutputKeys.INDENT, "yes");
-        tr.setOutputProperty(OutputKeys.METHOD, "xml");
-        tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        // tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
-        tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+      File temp = File.createTempFile("metadata", "xml", AppProperties.getTempDirectory());
 
-        // send DOM to file
-        tr.transform(new DOMSource(document), new StreamResult(fos));
-
-      }
-      catch (TransformerException te)
+      try (FileOutputStream fos = new FileOutputStream(temp))
       {
-        logger.error("Unexpected error while generating IDM metadata.", te);
-        throw new ProgrammingErrorException(te);
+        try
+        {
+          Transformer tr = TransformerFactory.newInstance().newTransformer();
+          tr.setOutputProperty(OutputKeys.INDENT, "yes");
+          tr.setOutputProperty(OutputKeys.METHOD, "xml");
+          tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+          // tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+          tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+          // send DOM to file
+          tr.transform(new DOMSource(document), new StreamResult(fos));
+
+        }
+        catch (TransformerException te)
+        {
+          logger.error("Unexpected error while generating IDM metadata.", te);
+          throw new ProgrammingErrorException(te);
+        }
       }
+      return temp;
     }
     catch (IOException e)
     {
       throw new ProgrammingErrorException(e);
     }
-    return temp;
   }
 
 }
