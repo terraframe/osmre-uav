c88660148eb9e54e00a7311973a0b379cdd75103
jsmethie <jsmethie@terraframe.com>
2024-09-20 15:26:33 -0600

    chore: knowstac integration

---
+++ uasdm-ui/src/app/site/component/know-stac-modal/know-stac-modal.component.ts
@@ -0,0 +1,50 @@
+///
+///
+///
+
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from '@angular/core';
+
+import { StacItem, StacProperty } from '@site/model/layer';
+import { KnowStacService } from '@site/service/know-stac.service';
+import { environment } from 'src/environments/environment';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+
+@Component({
+	selector: 'know-stac-modal',
+	templateUrl: './know-stac-modal.component.html',
+	styleUrls: []
+})
+export class KnowStacModalComponent implements OnInit, OnDestroy {
+
+
+
+	@Input() item: StacItem;
+
+	@Input() properties: StacProperty[] = null;
+
+
+	context: string;
+
+	constructor(private service: KnowStacService, public bsModalRef: BsModalRef, private modalService: BsModalService) {
+		this.context = environment.apiUrl;
+	}
+
+	init(item: StacItem, properties: StacProperty[]): void {
+		this.item = item;
+		this.properties = properties;
+
+		if (this.properties == null) {
+			this.service.properties().then(props => {
+				this.properties = props;
+			});
+		}
+	}
+
+	ngOnInit(): void {
+
+	}
+
+	ngOnDestroy(): void {
+	}
+}
