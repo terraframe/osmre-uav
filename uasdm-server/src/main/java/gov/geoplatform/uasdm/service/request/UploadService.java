4035d64c4a881fc59e55ce13bbc9c2d8d90428df
jsmethie <jsmethie@terraframe.com>
2025-11-03 11:11:48 -0700

    chore: parameterized chunk clean up for tus server

--- uasdm-server/src/main/java/gov/geoplatform/uasdm/service/request/UploadService.java
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/service/request/UploadService.java
@@ -2,7 +2,6 @@ package gov.geoplatform.uasdm.service.request;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
