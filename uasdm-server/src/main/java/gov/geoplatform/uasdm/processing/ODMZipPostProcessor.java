a79f922f9a5a654eee9c818de3680a90bfd0ef9d
jsmethie <jsmethie@terraframe.com>
2025-11-24 10:45:58 -0700

    chore: fixed issue with metadata file being included in the files which generate a product

--- uasdm-server/src/main/java/gov/geoplatform/uasdm/processing/ODMZipPostProcessor.java
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/processing/ODMZipPostProcessor.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2020 The Department of Interior
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package gov.geoplatform.uasdm.processing;
 
@@ -126,9 +126,13 @@ public class ODMZipPostProcessor
       list = processingTask.getFileList();
     }
 
-    List<DocumentIF> raws = collection.getDocuments().stream().filter(doc -> {
-      return doc.getS3location().contains("/raw/");
-    }).collect(Collectors.toList());
+    List<DocumentIF> raws = collection.getDocuments().stream()//
+        .filter(doc -> {
+          return doc.getS3location().contains("/raw/");
+        }).filter(doc -> {
+          // Exclude the metadata xml file
+          return !doc.getS3location().endsWith(".xml");
+        }).collect(Collectors.toList());
 
     for (DocumentIF raw : raws)
     {
