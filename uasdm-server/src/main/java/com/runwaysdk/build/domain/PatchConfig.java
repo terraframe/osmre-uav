cea615efe9e0d48019bab2aa32f9e57bed1edc4c
jsmethie <jsmethie@terraframe.com>
2026-01-08 08:52:14 -0700

    chore: updated patch config to get rid of class reference

--- uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
+++ uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
@@ -3,12 +3,6 @@ package com.runwaysdk.build.domain;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-import gov.geoplatform.uasdm.LogErrorFilter;
-import gov.geoplatform.uasdm.service.UASDMLoginGuard;
-import gov.geoplatform.uasdm.service.request.IDMSessionService;
-import gov.geoplatform.uasdm.service.request.UploadService;
-import net.geoprism.spring.web.JsonExceptionHandler;
-
 @ComponentScan(basePackages = { //
     "net.geoprism.spring", //
     "gov.geoplatform.uasdm.service", //
@@ -17,12 +11,12 @@ import net.geoprism.spring.web.JsonExceptionHandler;
     "net.geoprism.registry.service.permission", //
     "gov.geoplatform.uasdm.service.business" //
 }, excludeFilters = { //
-    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {  //
-        UploadService.class, //
-        JsonExceptionHandler.class, //
-        IDMSessionService.class, //
-        UASDMLoginGuard.class, //
-        LogErrorFilter.class //
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = {  //
+        ".*UploadService", //
+        ".*JsonExceptionHandler", //
+        ".*IDMSessionService", //
+        ".*UASDMLoginGuard", //
+        ".*LogErrorFilter" //
     })
 })
 public class PatchConfig
