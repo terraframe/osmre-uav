24fd53a6f401c800c5290bcf58489af3b7f3e914
jsmethie <jsmethie@terraframe.com>
2026-01-08 09:43:13 -0700

    chore: updated patch exclusions

--- uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
+++ uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.FilterType;
         ".*UploadService", //
         ".*JsonExceptionHandler", //
         ".*IDMSessionService", //
+        ".*SessionService", //
         ".*UASDMLoginGuard", //
         ".*LogErrorFilter" //
     })
