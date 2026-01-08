4b5ffc2681352cc1a42de4ce94210a18131b3a61
jsmethie <jsmethie@terraframe.com>
2026-01-08 07:18:10 -0700

    chore: updated exclusion list

--- uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
+++ uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
@@ -3,13 +3,27 @@ package com.runwaysdk.build.domain;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
+import gov.geoplatform.uasdm.LogErrorFilter;
+import gov.geoplatform.uasdm.service.UASDMLoginGuard;
+import gov.geoplatform.uasdm.service.request.IDMSessionService;
 import gov.geoplatform.uasdm.service.request.UploadService;
 import net.geoprism.spring.web.JsonExceptionHandler;
 
 @ComponentScan(basePackages = { //
-    "net.geoprism.spring", "gov.geoplatform.uasdm.service", "net.geoprism.registry.service.request", "net.geoprism.registry.service.business", "net.geoprism.registry.service.permission", "gov.geoplatform.uasdm.service.business" //
+    "net.geoprism.spring", //
+    "gov.geoplatform.uasdm.service", //
+    "net.geoprism.registry.service.request", //
+    "net.geoprism.registry.service.business", //
+    "net.geoprism.registry.service.permission", //
+    "gov.geoplatform.uasdm.service.business" //
 }, excludeFilters = { //
-    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { UploadService.class, JsonExceptionHandler.class }) //
+    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {  //
+        UploadService.class, //
+        JsonExceptionHandler.class, //
+        IDMSessionService.class, //
+        UASDMLoginGuard.class, //
+        LogErrorFilter.class //
+    })
 })
 public class PatchConfig
 {
