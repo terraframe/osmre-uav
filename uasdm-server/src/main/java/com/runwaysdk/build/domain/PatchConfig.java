7307b21dd9a34cd5c2dcb969fb30375a035fa326
jsmethie <jsmethie@terraframe.com>
2026-01-07 20:58:15 -0700

    chore: updated exclusion list

--- uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
+++ uasdm-server/src/main/java/com/runwaysdk/build/domain/PatchConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 import gov.geoplatform.uasdm.service.request.UploadService;
+import net.geoprism.spring.web.JsonExceptionHandler;
 
 @ComponentScan(basePackages = { //
     "net.geoprism.spring", "gov.geoplatform.uasdm.service", "net.geoprism.registry.service.request", "net.geoprism.registry.service.business", "net.geoprism.registry.service.permission", "gov.geoplatform.uasdm.service.business" //
 }, excludeFilters = { //
-    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { UploadService.class }) //
+    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { UploadService.class, JsonExceptionHandler.class }) //
 })
 public class PatchConfig
 {
