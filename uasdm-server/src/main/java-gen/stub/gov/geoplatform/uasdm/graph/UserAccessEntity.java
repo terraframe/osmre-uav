3968bc9097964ec2c212b7546eaf3fb8699a399c
jsmethie <jsmethie@terraframe.com>
2026-02-10 10:30:09 -0700

    chore: Better logging for user access errors

--- uasdm-server/src/main/java-gen/stub/gov/geoplatform/uasdm/graph/UserAccessEntity.java
+++ uasdm-server/src/main/java-gen/stub/gov/geoplatform/uasdm/graph/UserAccessEntity.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2020 The Department of Interior
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package gov.geoplatform.uasdm.graph;
 
@@ -76,7 +76,7 @@ public class UserAccessEntity extends UserAccessEntityBase
   {
     if (component instanceof UasComponent && !hasAccess((UasComponent) component))
     {
-      GenericException ex = new GenericException();
+      GenericException ex = new GenericException("User does not have access to component [" + component.getOid() + "]");
       ex.setUserMessage("Unable to find a component an id of [" + component.getOid() + "]");
       throw ex;
     }
