64a06869900987a7a32c7ab2670cd476ca3e24d8
jsmethie <jsmethie@terraframe.com>
2026-02-13 16:37:18 -0700

    chore: multipart upload support for spring 6.2

--- uasdm-server/src/main/java/gov/geoplatform/uasdm/SpringAppConfig.java
+++ uasdm-server/src/main/java/gov/geoplatform/uasdm/SpringAppConfig.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2020 The Department of Interior
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package gov.geoplatform.uasdm;
 
@@ -52,21 +52,19 @@ import net.geoprism.spring.web.JsonExceptionHandler;
     "gov.geoplatform.uasdm.controller", //
     "gov.geoplatform.uasdm.service", //
     "gov.geoplatform.uasdm.service.business", //
-    "gov.geoplatform.uasdm.maintenance"
-}, excludeFilters = { //
-    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "net.geoprism.registry.service.SessionFilter"), //
-    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "net.geoprism.registry.controller.*"), //
+    "gov.geoplatform.uasdm.maintenance" }, excludeFilters = { //
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "net.geoprism.registry.service.SessionFilter"), //
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "net.geoprism.registry.controller.*"), //
 })
 public class SpringAppConfig implements WebMvcConfigurer
 {
   @Bean(name = "multipartResolver")
   public MultipartResolver multipartResolver()
   {
-    MultipartResolver multipartResolver = new StandardServletMultipartResolver();
-//    multipartResolver.setMaxUploadSize(-1);
-    return multipartResolver;
+    // multipartResolver.setMaxUploadSize(-1);
+    return new StandardServletMultipartResolver();
   }
-  
+
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry)
   {
